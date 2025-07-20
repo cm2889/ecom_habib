dd4127ead705f3252fc9978290006516aeeb6aeb
monirul89 <monirul89@gmail.com>
2025-07-20 18:30:37 +0600

    add frontend app

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('backend/', include('backend.urls')),
+    path('', include('frontend.urls')),
 ]
 
 if settings.DEBUG:
