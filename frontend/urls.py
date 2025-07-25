caf71e7b04a0ff2ff9e5e75248d14a2bbc32b246
monirul89 <monirul89@gmail.com>
2025-07-25 09:24:37 +0600

    update

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/<str:slug>/', views.product_show, name='product_show'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# home
 def home(request):
     
     return render(request, 'home/index.html')
+
+# product_details
+def product_show(request, slug):
+    
+    return render(request, 'products/product_show.html')
